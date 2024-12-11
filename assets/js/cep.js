c8b8de76ae85f7af993d5b00d83d46bcc70b5a6e
traicygodoy <traicy.godoy@hotmail.com>
2024-12-11 14:30:54 -0300

    alterando js

--- assets/js/cep.js
+++ assets/js/cep.js
@@ -1,29 +1,31 @@
-document.getElementById("cep").addEventListener("blur", function () {
-    const cep = this.value.replace(/\D/g, "");
-    if (cep.length === 8) {
-        fetch(`viacep.com.br/ws/01001000/json/`)
-            .then(response => {
-                if (!response.ok) throw new Error("CEP não encontrado.");
-                return response.json();
-            })
-            .then(data => {
-                if (data.erro) {
-                    alert("CEP não encontrado.");
-                    return;
-                }
 
-            
-                document.getElementById("endereco").value = data.logradouro || "";
-                document.getElementById("bairro").value = data.bairro || "";
-                document.getElementById("cidade").value = data.localidade || "";
-                document.getElementById("estado").value = data.uf || "";
-            })
-            .catch(error => {
-                console.error("Erro ao buscar o CEP:", error.message);
-                alert("Erro ao buscar o CEP.");
-            });
-    } else {
-        alert("Por favor, insira um CEP válido com 8 dígitos.");
-    }
-});
 
+function buscarEndereco() {
+
+    var cep = document.getElementById("ceptxt").value; 
+
+    var url = "https://brasilapi.com.br/api/cep/v2/" + cep;
+
+    fetch(url)
+        .then(res => res.json()) 
+        .then(res => {
+            montarResultado(res);
+        })
+} 
+
+function montarResultado (objetoEndereco){
+
+    var dados = "<dados>";
+    
+    dados += "nome" + objetoEndereco.street;
+    dados += "bairro" + objetoEndereco.neighborhood;
+    dados += "cidade" + objetoEndereco.city;
+    
+    dados += "</dados>";
+
+    document.getElementById("dados").innerHTML = dados;
+
+}
+
+
+        
